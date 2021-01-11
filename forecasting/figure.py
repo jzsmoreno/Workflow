21db0ce0e981d04ee6e7b31faf71b3faa7c56d68
jzsmoreno <42299052+jzsmoreno@users.noreply.github.com>
2021-01-10 22:34:32 -0600

    Update figure.py

--- forecasting/figure.py
+++ forecasting/figure.py
@@ -11,11 +11,3 @@ def plot_series(df_series, data_pred, i = 0):
 def plot_time_series(df, i = 0):
     fig = px.line(df.iloc[:, i])
     return fig
-
-def plot_displot(df_series, data_pred, i = 0):
-    df_series['prediction'] = data_pred[i, :, 0]
-    # plotly figure
-    values = [i, -1]
-    fig = px.histogram(df_series, y=df_series.iloc[:, values].columns, color="sex",
-                   marginal="box", # or violin, rug)
-    return fig
