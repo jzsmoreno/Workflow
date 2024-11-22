6712a3b3a2e9d135cd4efeafd8b4356ec9dc2c9b
jzsmoreno <42299052+jzsmoreno@users.noreply.github.com>
2024-11-22 13:02:02 -0600

    [FIX] recombine_and_split function

--- heuristic/grasp.py
+++ heuristic/grasp.py
@@ -1,4 +1,5 @@
 import random
+
 import numpy as np
 import pandas as pd
 from numba import jit, njit, prange
--- heuristic/tools.py
+++ heuristic/tools.py
@@ -1,74 +1,75 @@
 import glob
 import os
 
+import numpy as np
+from numpy import loadtxt
+
 
 def recombine_and_split(pattern):
-    # Usar glob para encontrar todos los archivos que coincidan con el patrón
-    part_files = glob.glob(pattern)
-    part_files.sort()  # Asegurarse de que estén en el orden correcto
+    try:
+        # Obtener la lista de archivos que coinciden con el patrón
+        part_files = glob.glob(pattern)
 
-    combined_split_lines = []
+        # Verificar que se han encontrado archivos
+        if not part_files:
+            print(f"No se encontraron archivos con el patrón {pattern}")
+            return None
+        input_file = pattern.replace("_part_*", "")
+        recombine_files(pattern, input_file)
 
-    # Recorrer cada archivo de partes y agregar sus líneas a combined_split_lines
-    for part_file in part_files:
-        with open(part_file, "r") as part:
-            lines = part.readlines()
+        # Convertir la lista de listas a un arreglo NumPy
+        combined_array = loadtxt(input_file)
 
-            # Filtrar y procesar solo líneas que contengan solo caracteres numéricos
-            for i, line in enumerate(lines):
-                line = line.strip()  # Eliminar espacios en blanco y saltos de línea
+        return combined_array.tolist()
 
-                split_line = line.split("\t")  # Dividir la línea por tabuladores
-
-                # Convertir cada valor a un número flotante si es posible
-                try:
-                    split_line = [float(x) for x in split_line]  # Intentar convertir a float
-                except ValueError:
-                    # Si ocurre un error en la conversión, se puede optar por dejar el valor original
-                    # o manejarlos de alguna manera especial (como poner un valor por defecto).
-                    print(f"Advertencia: Línea no convertible a flotantes: {i}")
-                    split_line = [
-                        None if x == "" else x for x in split_line
-                    ]  # O manejarlo como None
-
-                combined_split_lines.append(split_line)
-
-    return combined_split_lines
+    except Exception as e:
+        print(f"Hubo un error al recombinar los archivos: {e}")
+        return None
 
 
 def split_file(input_file, chunk_size):
     with open(input_file, "r") as f:
         part_num = 0
-        buffer = ""
         while True:
+            # Leer un chunk de tamaño específico
             chunk = f.read(chunk_size)
             if not chunk:
-                break
-            buffer += chunk
-            lines = buffer.splitlines()
-            # If buffer contains enough lines, write them to a part file
-            if len(lines) > 1:  # Ensure there is at least one full line
-                with open(f"{input_file}_part_{part_num}", "w") as part_file:
-                    part_file.write("\n".join(lines))
-                buffer = lines[-1]  # Keep the last line as the start of the next chunk
-                part_num += 1
+                break  # Si ya no hay más contenido, terminamos
+            # Escribir el chunk en un archivo separado
+            with open(f"{input_file}_part_{part_num}", "w") as part_file:
+                part_file.write(chunk)
+            part_num += 1
 
 
 def recombine_files(pattern, output_file):
-    part_files = glob.glob(pattern)
-    part_files.sort()
-    with open(output_file, "w") as output:
-        for part_file in part_files:
-            with open(part_file, "r") as part:
-                output.write(part.read())
+    try:
+        # Obtener la lista de archivos que coinciden con el patrón
+        part_files = glob.glob(pattern)
+
+        # Verificar que se han encontrado archivos
+        if not part_files:
+            print(f"No se encontraron archivos con el patrón {pattern}")
+            return
+
+        # Ordenar los archivos para asegurarse de que se recombinen en el orden correcto
+        part_files.sort()
+
+        # Abrir el archivo de salida en modo escritura
+        with open(output_file, "w") as output:
+            for part_file in part_files:
+                with open(part_file, "r") as part:
+                    output.write(part.read())
+
+        print(f"Archivo recombinado guardado en {output_file}")
+
+    except Exception as e:
+        print(f"Hubo un error al recombinar los archivos: {e}")
 
 
 if __name__ == "__main__":
     input_file = "heuristic/evalu.txt"
     # Define the pattern that matches your part files
     pattern = "heuristic/evalu.txt_part_*"  # Ajustar patrón según el nombre de las partes
-
-    recombine_files(pattern, input_file)
 
     split_file(input_file, 50 * 1024 * 1024)  # Dividir en partes de 100MB
 
