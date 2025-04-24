2567dc99b1a6afcb1303c0af259972037ece6039
jzsmoreno <42299052+jzsmoreno@users.noreply.github.com>
2025-04-24 00:23:56 -0600

    [FIX] black dependency

--- dataframe_partial_join/dataframe_partial_join/__init__.py
+++ dataframe_partial_join/dataframe_partial_join/__init__.py
@@ -1,7 +1,7 @@
 """
 Authors: J. A. Moreno-Guerra
 Last modification: 12/31/2022
-Corresponding author: jzs.gm27@gmail.com 
+Corresponding author: jzs.gm27@gmail.com
 """
 
 from dataframe_partial_join.main import *
